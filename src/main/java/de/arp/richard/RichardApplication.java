bc96a9d35e59660d560690b65590f878db0623fb
Armin Pfarr <armin.pfarr@gmail.com>
2017-11-15 08:56:40 +0100

    Initial commit

---
+++ src/main/java/de/arp/richard/RichardApplication.java
@@ -0,0 +1,12 @@
+package de.arp.richard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichardApplication.class, args);
+	}
+}
